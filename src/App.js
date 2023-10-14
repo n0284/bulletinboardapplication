c130660cb1fa187a7cc80efc5af511394e1be479
rassu <senyopan@hotmail.com>
2023-10-14 23:00:12 +0900

    基礎1-02まで動作確認済

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [threadsList, setThreadsList] = useState();
+
+  useEffect(() => {
+    fetch("https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads")
+      .then((response) => response.json())
+      .then((data) => setThreadsList(data));
+  }, []);
+
+  // useEffectが動いた後にthreadsListを取得するためのメソッド
+  const threadsListDisplay = () => {
+    if (threadsList !== undefined) {
+      return threadsList.map((threads, id) => (
+        <li key={id}>{threads.title}</li>
+      ));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <ul>{threadsListDisplay()}</ul>
     </div>
   );
 }
